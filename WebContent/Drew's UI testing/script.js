4e8cb651bba458db3acd8e3a938a338ec906e1c0
Andrew Brantley <brantley@us.ibm.com>
2016-05-18 16:34:31 -0500

    Added animations
    animation to return pop up

--- WebContent/Drew's UI testing/script.js
+++ WebContent/Drew's UI testing/script.js
@@ -33,29 +33,35 @@ function addButtons(numberOfButtons)
 	}
 }
 
-var isOpen = false;
-var oldState = {
-	"left":0,
-	"top":0,
-};
-function openButton(buttonClicked)
+var oldState = {};
+var placeHolders = {};
+function openButton(buttonClicked) // for some reason I cant click them twice
 {
-	if( buttonClicked.classList.contains("open") || isOpen )
-	{		
-		buttonClicked.parentElement.removeChild(buttonClicked);
-		document.getElementById("assetsSubSlider").appendChild(buttonClicked);
-		
-		buttonClicked.style.position = oldState.position
-		buttonClicked.style.zIndex = oldState.zIndex		
-		buttonClicked.style.left = oldState.left
-		buttonClicked.style.top = oldState.top
-		
-		//*/
+	var parent = buttonClicked.parentElement
+	if( buttonClicked.classList.contains("open"))
+	{			
 		buttonClicked.classList.remove("open");
-		buttonClicked.classList.add("closed");
-		//*/
 		
-		buttonClicked.style.zIndex = 1	
+		setTimeout( function(){			
+			var placeHolder = placeHolders[ buttonClicked.id+"PlaceHolder" ];
+		
+			assetsSubSlider.appendChild( placeHolder )
+			placeHolder.classList.remove("hidden")
+			placeHolder.parentElement.removeChild( placeHolder );
+			parent.removeChild(buttonClicked);
+			document.getElementById("assetsSubSlider").appendChild(buttonClicked);
+		
+		
+			//*
+			buttonClicked.style.position = oldState.position
+			buttonClicked.style.zIndex = oldState.zIndex		
+			buttonClicked.style.left = oldState.left
+			buttonClicked.style.top = oldState.top
+			//*/
+		
+			buttonClicked.style.zIndex = 1	
+		}, 5000 ) // this needs to be the same as the animation time for shrinking
+
 	}
 	else
 	{		
@@ -66,27 +72,39 @@ function openButton(buttonClicked)
 		console.log("left "+buttonClicked.getBoundingClientRect().left+" & "+left)
 		console.log("top "+buttonClicked.getBoundingClientRect().top+" & "+top)
 		
-		oldState[position] = "fixed";
-		oldState[zIndex] = 2;
-		oldState[left] = left - 8;
-		oldState[top] = top - 8;
+		oldState["position"] = "";
+		oldState["zIndex"] = 2;
+		oldState["left"] = left - 8;
+		oldState["top"] = top - 8;
 		
 		buttonClicked.style.position = "fixed"
 		buttonClicked.style.zIndex = 2		
-		buttonClicked.style.left = left - 8; // THIS HAS TO BE THE SAME AS THE MARGIN VALUE IN THE CSS
-		buttonClicked.style.top = top -8;
+		buttonClicked.style.left = left - 8; // THIS number being subtracted HAS TO BE THE SAME AS THE MARGIN VALUE IN THE CSS
+		buttonClicked.style.top = top -8;		
 		
-		buttonClicked.parentElement.removeChild(buttonClicked);
+		var buttonPlaceHolder = buttonClicked.cloneNode();
+		buttonPlaceHolder.id = buttonPlaceHolder.id+"PlaceHolder";
+		buttonPlaceHolder.style.position = "static";
+		console.log( buttonPlaceHolder );
+		parent.appendChild(buttonPlaceHolder);
+		
+		console.log( buttonPlaceHolder.getBoundingClientRect() );
+		
+		//buttonPlaceHolder.classList.add("hidden");
+		buttonPlaceHolder.style.transition = "all 0s"
+		buttonPlaceHolder.style.background = "rgba(0,0,0,0)"
+		placeHolders[buttonPlaceHolder.id] = buttonPlaceHolder;
+		
+		
+		parent.removeChild(buttonClicked);
 		document.getElementsByTagName("body")[0].appendChild(buttonClicked);
 		
 		//*/
 		setTimeout(function(){ // i have no idea why this needs a timetout
 			buttonClicked.classList.add("open");
-			buttonClicked.classList.remove("closed");			
 		}, 0);
 		//*/
 	}
-	isOpen = !isOpen;
 }
 
 function buttonclick()
@@ -110,3 +128,25 @@ function toggleClass(e, className)
 		e.classList.add(className);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
