b0eb54bb3b954eaf7602e7d25cfbd1449f1a4877
Andrew Brantley <brantley@us.ibm.com>
2016-06-21 16:22:06 -0500

    backend manager started getting live data from back end

---
+++ DB Manager/helperFunctions.js
@@ -0,0 +1,124 @@
+var runFromLocalStorage = window.location.href.indexOf("file://") >= 0;
+
+function open_in_new_tab(url)
+{
+	var win = window.open(url, '_blank');
+	win.focus();
+}
+
+function pxToEm( px )
+{
+	//console.log("IN PX TO EM");
+	px += "";
+	px = px.split("px").join(""); // TODO chnage to work with regex?
+	px = parseFloat(px)
+	return ( 1/divForFindingEMSize.getBoundingClientRect().width ) * px;
+}
+	
+function emToPx( em )
+{
+	//console.log("IN EM TO PX");
+	em += "";
+	em = em.split("em").join("").split("rem").join(""); // TODO chnage to work with regex?
+	em = parseFloat(em)
+	return ( divForFindingEMSize.getBoundingClientRect().width/1 ) * em;
+}
+
+function toggleClass(e, className)
+{
+	if( (typeof e) == "number" || (typeof e) == "string" || (typeof e) == "function")
+	{
+		return;
+	}
+	
+	if(e.classList.contains(className))
+	{
+		e.classList.remove(className);
+	}
+	else
+	{
+		e.classList.add(className);
+	}
+}
+
+function httpRequest(url, callback, method)
+{
+	async = true;
+	method = method || "GET";
+	
+	var xhttp = new XMLHttpRequest();
+	
+	
+		console.log(52)
+	xhttp.onreadystatechange = function() 
+	{
+		console.log("xhttp.readyState is "+xhttp.readyState)
+		console.log("xhttp.status is "+xhttp.status)
+		if (xhttp.readyState == 4 && xhttp.status == 200) 
+		{
+			//console.log( JSON.parse(xhttp.responseText) );
+			
+			if(callback !== undefined)
+			{
+				callback( xhttp.responseText );
+			}
+		}
+	};
+	console.log("requesting *"+url+"*");
+	xhttp.open(method, url, async);
+	try{
+		xhttp.send();
+	}
+	catch(e){
+		console.log("***Using offline data");
+		callback( localStorage.getItem("search_results") )
+	}
+	return xhttp;
+}
+
+var pointsObj = [
+	{"x":0, "y":0},
+	{"x":10, "y":0},
+	{"x":20, "y":0},
+	{"x":10, "y":20}
+]
+
+function makeSVGImage(pointsArr, width, height)
+{
+	var dStr = "";
+	for(var i=0; i<pointsArr.length; i++)
+	{
+		dStr += i===0 ? "M" : " L";
+		
+		if( pointsArr[i].x <= 1 ) // this is so you can use relitive fractions to the size of the points
+		{
+			pointsArr[i].x *= width;
+		}				
+		if( pointsArr[i].y <= 1 ) // this is so you can use relitive fractions to the size of the points
+		{
+			pointsArr[i].y *= height;
+		}
+		
+		dStr += pointsArr[i].x + " " + pointsArr[i].y; 
+	}
+	return dStr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- DB Manager/script.js
+++ DB Manager/script.js
@@ -1,3 +1,5 @@
+var currentData = undefined;
+
 function clickTab(e)
 {	
 	var removeSelected = document.querySelectorAll(".tab.selected");
@@ -36,7 +38,100 @@ function optionsContinue()
 	document.querySelector( "#"+clicked.getAttribute("tab") ).click();
 }
 
+function test()
+{
+	//httpRequest(config.url+"/PSAP/dbCategories?action=search&type=SOLUTION&parentid="+"01", function(d){console.log("d is "+d);});
+	//httpRequest(config.url+"/PSAP/dbCategories?action=search&type=OFERING&parentid="+"101", function(d){console.log("d is "+d);});
+	httpRequest(config.url+"/PSAP/dbAssets?action=search&SubmittedBy="+"edgingtn@us.ibm.com", function(d){console.log("d is "+d);});
+	
+	//httpRequest(config.url+"/PSAP/dbCategories?type=OFERING&action=search&parentid="+"101", getSelectData);
+}
 
+function selectChange(changedSelect, nextId, type)
+{
+	var nextElement = document.querySelector("#"+nextId);
+	
+	if(changedSelect.value == "Make a selection" || changedSelect.value == "There were no results")
+	{
+		nextElement.innerHTML = "<option>Make a "+type.toLowerCase()+" selection</option>";
+	}
+	else
+	{	
+		var URLSegment = undefined;
+		var parentID = "";
+	
+		if( type.toLowerCase() === "categories" )
+		{
+			console.log("c "+type.toLowerCase())
+			URLSegment = config.categoriesURL;
+			console.log("changedSelect is... ");console.log(changedSelect.value);
+			parentID = config.categories[ changedSelect.value ]; //fix this
+		}
+		else if( type.toLowerCase() === "solutions" )
+		{
+			console.log("s "+type.toLowerCase())
+			URLSegment = config.solutionsURL;
+			parentID = getItemObject( changedSelect.value ).catid;
+		}
+		else if( type.toLowerCase() === "offerings" )
+		{
+			console.log("o "+type.toLowerCase())
+			URLSegment = config.offeringsURL;
+			parentID = "edgingtn@us.ibm.com";
+		}
+	
+		httpRequest(config.url+URLSegment+parentID, function(data){ currentData=JSON.parse(data); setSelect( nextElement,  currentData);});
+	}
+}
+
+function getSelectData(data, nextElement)
+{
+	data = JSON.parse(data);
+	console.log("\t\tdata is "+data);
+	console.log("\t\tdata.result is "+data.result);
+	setSelect( nextElement, data )
+}
+
+function setSelect(select, data)
+{
+	data = data.result;
+	console.log("data is "+data);
+	
+	select.innerHTML = "";
+	
+	var option = document.createElement("option");
+	option.innerHTML = data.length > 0 ? "Make a selection": "There were no results";
+	select.appendChild(option);
+	
+	for(var i=0; i<data.length; i++)
+	{
+		var option = document.createElement("option");
+		option.innerHTML = data[i].catdisplayname;
+		option.name = data[i].catid;
+		select.appendChild(option);
+	}
+}
+
+function populateEdit( changedSelect, tabId)
+{
+	var obj = getItemObject( changedSelect.value );
+	document.querySelector('#'+ tabId +' input[placeholder=Name]').value = obj.catdisplayname;
+	document.querySelector('#'+ tabId +' input[placeholder=Description]').value = obj.catdesc;
+}
+
+function getItemObject(itemName)
+{
+	for(var i=0; i<currentData.result.length; i++)
+	{
+		if( currentData.result[i].catdisplayname === itemName )
+		{
+			console.log("returning"+ currentData.result[i].catid);
+			return currentData.result[i];
+		}
+	}
+	console.log("didnt find requested solution")
+	console.log("currentData is...");console.log(currentData);
+}
 
 
 
